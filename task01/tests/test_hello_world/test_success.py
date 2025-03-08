47a95fbfdd94ec3dd72fe70ff32d3be2c11dace1
sita2004 <151060405+sita2004@users.noreply.github.com>
2025-03-08 13:17:10 +0530

    Update test_success.py

--- task01/tests/test_hello_world/test_success.py
+++ task01/tests/test_hello_world/test_success.py
@@ -4,5 +4,9 @@ from tests.test_hello_world import HelloWorldLambdaTestCase
 class TestSuccess(HelloWorldLambdaTestCase):
 
     def test_success(self):
+        expected_response = {
+            "statusCode": 200,
+            "message": "Hello from Lambda"
+        }
         response = self.HANDLER.handle_request(dict(), dict())
-        self.assertEqual(response["statusCode"], 200)
+        self.assertEqual(response, expected_response)
