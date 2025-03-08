e57074dd8065da498c326d575bb5f82d53a8504a
sita2004 <151060405+sita2004@users.noreply.github.com>
2025-03-08 13:18:01 +0530

    Update handler.py

--- task01/src/lambdas/hello_world/handler.py
+++ task01/src/lambdas/hello_world/handler.py
@@ -7,23 +7,16 @@ _LOG = get_logger(__name__)
 class HelloWorld(AbstractLambda):
 
     def validate_request(self, event) -> dict:
-        """
-        This method can be used to validate incoming requests.
-        Currently, it's not implemented.
-        """
         pass
 
     def handle_request(self, event, context):
         """
-        This method handles the incoming request event.
-        Currently, it returns a fixed HTTP 200 response.
+        Explain incoming event here
         """
-        # todo: implement business logic here
-        _LOG.info("Handling request with event: %s", event)
-
+        # Implementing business logic to return JSON response
         response = {
             "statusCode": 200,
-            "body": '{"message": "Hello, World!"}'
+            "message": "Hello from Lambda"
         }
         return response
 
@@ -32,8 +25,4 @@ HANDLER = HelloWorld()
 
 
 def lambda_handler(event, context):
-    """
-    AWS Lambda's entry point.
-    This function delegates the processing to the AbstractLambda.
-    """
     return HANDLER.lambda_handler(event=event, context=context)
