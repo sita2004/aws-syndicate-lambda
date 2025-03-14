a6cc4a5881168f8c49261fb2701942b4595fdf84
sita2004 <151060405+sita2004@users.noreply.github.com>
2025-03-14 12:54:43 +0530

    Update SnsHandler.java

--- task04/jsrc/main/java/com/task04/SnsHandler.java
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -2,38 +2,32 @@ package com.task04;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.events.SnsEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @LambdaHandler(
-		lambdaName = "sns_handler",
-		roleName = "sns_handler-role",
-		isPublishVersion = true,
-		aliasName = "${lambdas_alias_name}",
-		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+        lambdaName = "sns_handler",
+        roleName = "sns_handler-role",
+        isPublishVersion = true,
+        aliasName = "learn",
+        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-public class SnsHandler implements RequestHandler<Map<String, Object>, Map<String, Object>> {
+@SnsEventSource(
+        targetTopic = "lambda_topic"
+)
+public class SnsHandler implements RequestHandler<Object, Map<String, Object>> {
 
-	public Map<String, Object> handleRequest(Map<String, Object> event, Context context) {
-		System.out.println("Received SNS event: " + event);
+    public Map<String, Object> handleRequest(Object request, Context context) {
+        context.getLogger().log("Received event: " + request.toString() + "\n");
 
-		// Extract messages
-		if (event.containsKey("Records")) {
-			List<Map<String, Object>> records = (List<Map<String, Object>>) event.get("Records");
-			for (Map<String, Object> record : records) {
-				Map<String, Object> sns = (Map<String, Object>) record.get("Sns");
-				String messageBody = (String) sns.get("Message");
-				System.out.println("SNS Message: " + messageBody);
-			}
-		}
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("statusCode", 200);
+        resultMap.put("body", "Processed SNS Messages Successfully");
 
-		Map<String, Object> resultMap = new HashMap<>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("body", "SNS Message Processed");
-		return resultMap;
-	}
+        return resultMap;
+    }
 }
