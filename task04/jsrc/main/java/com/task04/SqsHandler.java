f1b3ac2dc57dec32cf35fb25f6440341ae23bead
sita2004 <151060405+sita2004@users.noreply.github.com>
2025-03-14 12:54:21 +0530

    Update SqsHandler.java

--- task04/jsrc/main/java/com/task04/SqsHandler.java
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -2,37 +2,33 @@ package com.task04;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @LambdaHandler(
 		lambdaName = "sqs_handler",
 		roleName = "sqs_handler-role",
 		isPublishVersion = true,
-		aliasName = "${lambdas_alias_name}",
+		aliasName = "learn",
 		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-public class SqsHandler implements RequestHandler<Map<String, Object>, Map<String, Object>> {
+@SqsTriggerEventSource(
+		targetQueue = "async_queue",
+		batchSize = 10
+)
+public class SqsHandler implements RequestHandler<Object, Map<String, Object>> {
 
-	public Map<String, Object> handleRequest(Map<String, Object> event, Context context) {
-		System.out.println("Received SQS event: " + event);
-
-		// Extract messages
-		if (event.containsKey("Records")) {
-			List<Map<String, Object>> records = (List<Map<String, Object>>) event.get("Records");
-			for (Map<String, Object> record : records) {
-				String messageBody = (String) record.get("body");
-				System.out.println("SQS Message: " + messageBody);
-			}
-		}
+	public Map<String, Object> handleRequest(Object request, Context context) {
+		context.getLogger().log("Received event: " + request.toString() + "\n");
 
 		Map<String, Object> resultMap = new HashMap<>();
 		resultMap.put("statusCode", 200);
-		resultMap.put("body", "SQS Message Processed");
+		resultMap.put("body", "Processed SQS Messages Successfully");
+
 		return resultMap;
 	}
 }
