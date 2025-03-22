16059257e7172b7e583c92bc90b3650a9794ebef
sita2004 <151060405+sita2004@users.noreply.github.com>
2025-03-22 22:31:22 +0530

    Delete task10 directory

--- task10/tests/test_processor/test_success.py
+++
@@ -1,8 +0,0 @@
-from tests.test_processor import ProcessorLambdaTestCase
-
-
-class TestSuccess(ProcessorLambdaTestCase):
-
-    def test_success(self):
-        self.assertEqual(self.HANDLER.handle_request(dict(), dict()), 200)
-
